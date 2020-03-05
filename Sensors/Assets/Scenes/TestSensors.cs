152e5c03271385bd7c4d9958bc550e0ff15358ed
aneeshdalvi <adalvi1@asu.edu>
2020-03-05 01:45:40 -0700

    Added Sensors Sprint1 Deliverable Project

---
+++ Sensors/Assets/Scenes/TestSensors.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Sensors1;
+using System;
+using UnityEngine.SceneManagement;
+
+public class TestSensors : MonoBehaviour
+{
+
+    public Button play;
+    public InputField input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+
+        play.onClick.AddListener(callSensorLibrary);
+    }
+
+    public void callSensorLibrary()
+    {
+        int SensorType = int.Parse(input.text);
+        Sensors1.Sensors sensor = new Sensors1.Sensors();
+        Debug.Log(sensor.chooseSensor(SensorType));
+        SceneManager.LoadScene("SensorTest");
+    }
+
+
+
+
+}
+
---
+++ Sensors/Assets/Scenes/proximitySensor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class proximitySensor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Cube;
+    public GameObject Cube1;
+    public float Distance;
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 10f;
+
+        if (Input.GetKey(KeyCode.W))
+            Cube.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if (Input.GetKey(KeyCode.S))
+            Cube.transform.Translate(-1 * Vector3.forward * Time.deltaTime * speed);
+
+        if (Input.GetKey(KeyCode.A))
+            Cube.transform.Rotate(0, -1, 0);
+
+        if (Input.GetKey(KeyCode.D))
+            Cube.transform.Rotate(0, 1, 0);
+
+        if (Cube != null && Cube1 != null)
+        {
+            Distance = Vector3.Distance(Cube.transform.position, Cube1.transform.position);
+            Debug.Log(string.Format("Distance between {0} and {1} is: {2}", Cube, Cube1, Distance));            
+        }
+        
+    }
+}
