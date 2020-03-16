ce3ce528a5ba7e6405785c9e727354f3ec5322c3
aneeshdalvi <adalvi1@asu.edu>
2020-03-16 00:07:14 -0700

    Added Camera sensor and Accelerometer sensor functionality

--- Sensors/Assets/Scenes/proximitySensor.cs
+++ Sensors/Assets/Scenes/proximitySensor.cs
@@ -8,16 +8,36 @@ public class proximitySensor : MonoBehaviour
     public GameObject Cube;
     public GameObject Cube1;
     public float Distance;
+    private float oldTime;
+    private float newTime;
+    private float newVelocity;
+    private float oldVelocity;
     void Start()
     {
 
         
     }
 
+    private float getAcceleration(Rigidbody cube)
+    {
+        newTime = Time.time;
+        newVelocity = cube.velocity.magnitude;
+        float acceleration = ((newVelocity - oldVelocity) / (newTime - oldTime));
+        oldTime = newTime;
+        oldVelocity = newVelocity;
+
+        return acceleration;
+    }
+
     // Update is called once per frame
     void Update()
     {
         float speed = 10f;
+        Camera camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        Rigidbody cube = GameObject.Find("Cube").GetComponent<Rigidbody>();        
+        float acceleration = getAcceleration(cube);
+
+        Debug.Log(string.Format("Acceration of {0} is {1}", Cube, acceleration));
 
         if (Input.GetKey(KeyCode.W))
             Cube.transform.Translate(Vector3.forward * Time.deltaTime * speed);
