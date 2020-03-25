4fb3320ded1a9b38716c97883bdd1857bd0cbfeb
venkata sai ram <veadala@asu.edu>
2020-03-24 20:06:45 -0700

    Updated the fields with new values

--- ExperimentalDesign/UIInterface/Assets/updatemessage.cs
+++ ExperimentalDesign/UIInterface/Assets/updatemessage.cs
@@ -7,9 +7,9 @@ using TMPro;
 public class updatemessage : MonoBehaviour
 {
 	public TMPro.TMP_Dropdown InputAlgorithm;
-	public TMPro.TMP_Dropdown MazeType;
 	public TMPro.TMP_Dropdown MazeSize;
 	public TMPro.TMP_Dropdown SensorType;
+	public TMPro.TMP_InputField Thresholdvalue;
 	public Text Result;
 	
     // Start is called before the first frame update
@@ -17,18 +17,27 @@ public class updatemessage : MonoBehaviour
     {
 	InputAlgorithm = GameObject.Find("Algorithm").GetComponent<TMPro.TMP_Dropdown>();
     string InputAlgorithmValue = InputAlgorithm.captionText.text;
-	MazeType = GameObject.Find("MazeType").GetComponent<TMPro.TMP_Dropdown>();
-    string MazeTypeValue = MazeType.captionText.text;
 	MazeSize = GameObject.Find("MazeSize").GetComponent<TMPro.TMP_Dropdown>();
     string MazeSizeValue = MazeSize.captionText.text;
+	string value = Thresholdvalue.text;
+	float thresholdval = float.Parse(value);
 	SensorType = GameObject.Find("SensorType").GetComponent<TMPro.TMP_Dropdown>();
     string SensorTypeValue = SensorType.captionText.text;
-	// result  =  GameObject.Find("Status_text").GetComponent<InputField>();
-	string updatedmessage="Simulation is running with " + InputAlgorithmValue + " as Algorithm Type, " + MazeTypeValue  + " as Maze Type, " + MazeSizeValue + " as Maze Size, " + SensorTypeValue + " as sensors.";
+	
+
+	  if (!string.IsNullOrEmpty(value)&&(thresholdval >= 0 && thresholdval <=1))
+   {
+    string updatedmessage="Simulation is running with " + InputAlgorithmValue + " as Algorithm Type, " + value  + " as Threshold Value, " + MazeSizeValue + " as Maze Size, " + SensorTypeValue + " as sensors.";
 	Result.text=updatedmessage;
+   }
+   else
+   {
+      string alertmessage = "Kindly enter the value of Placement Threshold or Enter values in range 0 to 1";
+	  Result.text=alertmessage;
+   }    
+}
+	//string updatedmessage="Simulation is running with " + InputAlgorithmValue + " as Algorithm Type, " + value  + " as Threshold Value, " + MazeSizeValue + " as Maze Size, " + SensorTypeValue + " as sensors.";
+	//Result.text=updatedmessage;
 
-    }
-
-   
    
 }
