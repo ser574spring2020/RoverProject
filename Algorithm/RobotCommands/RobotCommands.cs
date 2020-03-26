e92adae75a1b2a7f6f0a6eb8a744b2568375a85a
MayankBatra005 <er.mayank005@gmail.com>
2020-03-25 23:34:22 -0700

    Unit testing before integrating with Datalayer

--- Algorithm/RobotCommands/RobotCommands.cs
+++ Algorithm/RobotCommands/RobotCommands.cs
@@ -10,53 +10,70 @@ hence generating the commands for robot's next movement
 */
 public class RobotCommands
 {
- public List<String> computeDirection(int[,] sensorData)
+    public static void Main()
     {
-		/* For unit testing
-		int[] dataBaseMatrix=new int[1];
-		int Left,North,South;
-		int right=Left=North=South=0;
-		dataBaseMatrix[right]=0;
-		*/
-		List<String> nextCommandList = new List<String>();
-
-        if ((sensorData[1,2] == 0) && (dataBaseMatrix[right] != 4))
+        int[,] sensorData = new int[3, 3];
+        sensorData[1, 2] = 0;
+        sensorData[0, 0] = 1;
+        sensorData[0, 1] = 1;
+        sensorData[0, 2] = 1;
+        sensorData[1, 0] = 1;
+        sensorData[1, 1] = 2;
+        sensorData[2, 1] = 1;
+        sensorData[2, 2] = 1;
+        sensorData[2, 0] = 1;
+		RobotCommands obj=new RobotCommands();
+        foreach (var item in obj.computeDirection(sensorData))
         {
-			nextCommandList.Add("East");
-			// update the current movement in DB and then save the sensor data 
-			saveSensorData(sensorData,1,2);
-			
+            Console.WriteLine("The nodes are:" + item);
         }
-        else if ((sensorData[1,0] == 0) && (dataBaseMatrix[Left] != 4))
+    }
+
+    public List<String> computeDirection(int[,] sensorData)
+    {
+        // For unit testing
+        int[] dataBaseMatrix = new int[1];
+        int Left, North, South;
+        int right = Left = North = South = 0;
+        dataBaseMatrix[right] = 0;
+
+        List<String> nextCommandList = new List<String>();
+
+        if ((sensorData[1, 2] == 0) && (dataBaseMatrix[right] != 4))
+        {
+            nextCommandList.Add("East");
+            // update the current movement in DB and then save the sensor data 
+            saveSensorData(sensorData, 1, 2);
+        }
+        else if ((sensorData[1, 0] == 0) && (dataBaseMatrix[Left] != 4))
         {
             nextCommandList.Add("West");
-			// update the current movement in DB and then save the sensor data 
-			saveSensorData(sensorData,1,0);
+            // update the current movement in DB and then save the sensor data 
+            saveSensorData(sensorData, 1, 0);
         }
-        else if ((sensorData[0,1] == 0) && (dataBaseMatrix[North] != 4))
+        else if ((sensorData[0, 1] == 0) && (dataBaseMatrix[North] != 4))
         {
-			nextCommandList.Add("North");
-			// update the current movement in DB and then save the sensor data 
-			saveSensorData(sensorData,0,1);
+            nextCommandList.Add("North");
+            // update the current movement in DB and then save the sensor data 
+            saveSensorData(sensorData, 0, 1);
         }
-        else if ((sensorData[2,1] == 0) && (dataBaseMatrix[South] != 4))
+        else if ((sensorData[2, 1] == 0) && (dataBaseMatrix[South] != 4))
         {
-			nextCommandList.Add("South");
-			// update the current movement in DB and then save the sensor data 
-			saveSensorData(sensorData,2,1);
+            nextCommandList.Add("South");
+            // update the current movement in DB and then save the sensor data 
+            saveSensorData(sensorData, 2, 1);
         }
         else
         {
-			// DepthFirstSearch findPath=new DepthFirstSearch();
-			// nextCommandList=findPath.findPathAdapter();
+            // DepthFirstSearch findPath=new DepthFirstSearch();
+            // nextCommandList=findPath.findPathAdapter();
         }
-		return nextCommandList;
-	}
-	
-	public void saveSensorData(int[,] sensorData, int rowPosition,int columnPosition)
-	{
-		//	save sensor data as it is
-		// update the rowPosition and columnPosition with 4 
-		
-	}
+        return nextCommandList;
+    }
+
+    public void saveSensorData(int[,] sensorData, int rowPosition, int columnPosition)
+    {
+        //	save sensor data as it is
+        // update the rowPosition and columnPosition with 4 
+    }
 }
