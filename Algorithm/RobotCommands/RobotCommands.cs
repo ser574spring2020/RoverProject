3aee34eb091b170f08fdd4b25d93d43abeb17ad3
MayankBatra005 <er.mayank005@gmail.com>
2020-03-25 23:13:47 -0700

    Bug fix

--- Algorithm/RobotCommands/RobotCommands.cs
+++ Algorithm/RobotCommands/RobotCommands.cs
@@ -12,28 +12,34 @@ public class RobotCommands
 {
  public List<String> computeDirection(int[,] sensorData)
     {
+		/* For unit testing
+		int[] dataBaseMatrix=new int[1];
+		int Left,North,South;
+		int right=Left=North=South=0;
+		dataBaseMatrix[right]=0;
+		*/
 		List<String> nextCommandList = new List<String>();
 
-        if ((sensorData[1,2])] == 0) && (dataBaseMatrix[right] != 4))
+        if ((sensorData[1,2] == 0) && (dataBaseMatrix[right] != 4))
         {
 			nextCommandList.Add("East");
 			// update the current movement in DB and then save the sensor data 
 			saveSensorData(sensorData,1,2);
 			
         }
-        else if ((sensorData[1,0])] == 0) && (dataBaseMatrix[Left] != 4))
+        else if ((sensorData[1,0] == 0) && (dataBaseMatrix[Left] != 4))
         {
             nextCommandList.Add("West");
 			// update the current movement in DB and then save the sensor data 
 			saveSensorData(sensorData,1,0);
         }
-        else if ((sensorData[0,1])] == 0) && (dataBaseMatrix[North] != 4))
+        else if ((sensorData[0,1] == 0) && (dataBaseMatrix[North] != 4))
         {
 			nextCommandList.Add("North");
 			// update the current movement in DB and then save the sensor data 
 			saveSensorData(sensorData,0,1);
         }
-        else if ((sensorData[2,1])] == 0) && (dataBaseMatrix[South] != 4))
+        else if ((sensorData[2,1] == 0) && (dataBaseMatrix[South] != 4))
         {
 			nextCommandList.Add("South");
 			// update the current movement in DB and then save the sensor data 
@@ -41,8 +47,8 @@ public class RobotCommands
         }
         else
         {
-			DepthFirstSearch findPath=new DepthFirstSearch();
-			nextCommandList=findPath.findPathAdapter();
+			// DepthFirstSearch findPath=new DepthFirstSearch();
+			// nextCommandList=findPath.findPathAdapter();
         }
 		return nextCommandList;
 	}
