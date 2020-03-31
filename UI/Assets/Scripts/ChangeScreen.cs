8e16ef0b6f5d53e608e0a6b4ce519fabc4c8c252
Vaibhav Bhasin <vbhasin2@asu.edu>
2020-03-30 17:50:53 -0700

    ExpeDesign dll integrated and running app with 2 screens

---
+++ UI/Assets/Scripts/ChangeScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeScreen : MonoBehaviour
+{
+   
+    public void MainScene()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+        
+    }
+}
+
