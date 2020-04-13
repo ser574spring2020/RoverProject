41fbf2be6918a6c463eb475a6363d469edd5f8d3
venkata sai ram <veadala@asu.edu>
2020-04-13 13:04:45 -0700

    Integrated with DB

---
+++ ExperimentalDesign/WholeProject-Dll/UIInterface - Dll/Assets/ExpDesignScript.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+using ExperimentalDesign;
+
+
+public class ExpDesignScript : MonoBehaviour
+{
+	public TMPro.TMP_Dropdown InputAlgorithm;
+	public TMPro.TMP_Dropdown MazeSize;
+	public TMPro.TMP_Dropdown SensorType;
+	public TMPro.TMP_InputField Thresholdvalue;
+	public Text Result;
+
+	// Start is called before the first frame update
+
+	public void Start()
+	{
+		InputAlgorithm.options.Clear();
+		MazeSize.options.Clear();
+		SensorType.options.Clear();
+		ExperimentalDesignClass experimentalDesign = new ExperimentalDesignClass();
+		List<string> options_list;
+
+		// Setting algorithm dropdown values
+		options_list = experimentalDesign.setAlgorithm();
+		foreach (string algorithm in options_list)
+		{
+			InputAlgorithm.options.Add(new TMP_Dropdown.OptionData() { text = algorithm });
+		}
+		InputAlgorithm.value = 1;
+		InputAlgorithm.value = 0;
+
+		// Setting Maze Type dropdown values
+
+
+		// Setting Maze size dropdown values
+		options_list = experimentalDesign.setMazeSize();
+		foreach (string maze_size in options_list)
+		{
+			MazeSize.options.Add(new TMP_Dropdown.OptionData() { text = maze_size });
+		}
+		MazeSize.value = 1;
+		MazeSize.value = 0;
+
+		// Setting Sensor Type dropdown values
+		options_list = experimentalDesign.setSensors();
+		foreach (string sensor in options_list)
+		{
+			SensorType.options.Add(new TMP_Dropdown.OptionData() { text = sensor });
+		}
+		SensorType.value = 1;
+		SensorType.value = 0;
+	}
+	public void updateText()
+	{
+		string value = "";
+		double thresholdval = 2.0F;
+		InputAlgorithm = GameObject.Find("Algorithm").GetComponent<TMPro.TMP_Dropdown>();
+		string InputAlgorithmValue = InputAlgorithm.captionText.text;
+		MazeSize = GameObject.Find("MazeSize").GetComponent<TMPro.TMP_Dropdown>();
+		string MazeSizeValue = MazeSize.captionText.text;
+
+
+
+		try
+		{
+			value = Thresholdvalue.text;
+			thresholdval = float.Parse(value);
+		}
+		catch (FormatException fe)
+		{
+			string alertmessage = "Kindly enter the value of Placement Threshold or Enter values in range 0 to 1";
+			Result.text = alertmessage;
+		}
+		SensorType = GameObject.Find("SensorType").GetComponent<TMPro.TMP_Dropdown>();
+		string SensorTypeValue = SensorType.captionText.text;
+
+
+		if (!string.IsNullOrEmpty(value) && (thresholdval >= 0 && thresholdval <= 1))
+		{
+
+			string updatedmessage = "Simulation is running with " + InputAlgorithmValue + " as Algorithm Type, " + value + " as Threshold Value, " + MazeSizeValue + " as Maze Size, " + SensorTypeValue + " as sensors.";
+			Result.text = updatedmessage;
+			database db = new database();
+			db.Insert(InputAlgorithmValue, MazeSizeValue, thresholdval, SensorTypeValue);
+		}
+		else
+		{
+			string alertmessage = "Kindly enter the value of Placement Threshold or Enter values in range 0 to 1";
+			Result.text = alertmessage;
+		}
+
+
+	}
+
+	public void changeScene(String sceneName)
+	{
+		Application.LoadLevel(sceneName);
+	}
+	//string updatedmessage="Simulation is running with " + InputAlgorithmValue + " as Algorithm Type, " + value  + " as Threshold Value, " + MazeSizeValue + " as Maze Size, " + SensorTypeValue + " as sensors.";
+	//Result.text=updatedmessage;
+
+
+}
---
+++ ExperimentalDesign/WholeProject-Dll/UIInterface - Dll/Assets/database.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExperimentalDesignDatabase;
+
+
+public class database : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ExperimentalDesignDb expdb = new ExperimentalDesignDb();
+        expdb.Initialize();   
+        //print(expdb.OpenConnection());
+    }
+
+    public void Insert(string algorithm, string mazetype, double thresholdvalue, string sensor) { 
+    
+        ExperimentalDesignDb expdb = new ExperimentalDesignDb();
+
+        expdb.Initialize();
+
+        //print("INSERT INTO experimental_results(algorithmtype, mazesize, tresholdfrequency,sensortype) VALUES ('" + algorithm + "','" + mazetype + "'," + thresholdvalue + ",'" + sensor + "');");
+
+        expdb.Insert("INSERT INTO experimental_results(AlgorithmType, MazeSize, ThresholdFrequency,SensorType) VALUES ('" + algorithm + "','"+mazetype + "',"+ thresholdvalue+",'"+ sensor+"');");
+
+
+    }
+
+    public void selectValuesfromDB(string yAxisValue, string InputAlgorithmValue, string MazeSizeValue,float Threshold, string SensorTypeValue)
+    {
+        ExperimentalDesignDb expdb = new ExperimentalDesignDb();
+
+        expdb.Initialize();
+        List<string> range = expdb.Select();
+        foreach (string item in range)
+        {
+            print(item);
+        }
+
+    }
+
+    // Update is called once per frame
+
+    public void UpdateAlgorithm(string InputAlgorithmvalue)
+    {
+        ExperimentalDesignDb expdb = new ExperimentalDesignDb();
+
+        expdb.Initialize();
+        //print(expdb.OpenConnection());
+        expdb.Update("UPDATE experimental_results SET AlgorithmType='" + InputAlgorithmvalue + "' WHERE ID IN (SELECT Max(ID) FROM experimental_results);;");
+        //print(expdb.UpdateAlgo());
+
+    }
+
+    public void UpdateSensor(string SensorValue)
+    {
+        ExperimentalDesignDb expdb = new ExperimentalDesignDb();
+
+        expdb.Initialize();
+        expdb.Update("UPDATE experimental_results SET SensorType='" + SensorValue + "' WHERE ID IN (SELECT Max(ID) FROM experimental_results);;");
+
+    }
+
+    public void UpdateMazeSize(string MazeSize)
+    {
+        ExperimentalDesignDb expdb = new ExperimentalDesignDb();
+
+        expdb.Initialize();
+        expdb.Update("UPDATE experimental_results SET MazeSize='" + MazeSize + "' WHERE ID IN (SELECT Max(ID) FROM experimental_results);;");
+
+    }
+
+    public void UpdateThreshold(float threshold)
+    {
+        ExperimentalDesignDb expdb = new ExperimentalDesignDb();
+
+        expdb.Initialize();
+        expdb.Update("UPDATE experimental_results SET ThresholdFrequency=" + threshold + " WHERE ID IN (SELECT Max(ID) FROM experimental_results);;");
+
+    }
+    void Update()
+    {
+
+    }
+}
