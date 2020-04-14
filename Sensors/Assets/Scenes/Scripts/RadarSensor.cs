97334af074afbd08eefedc129558ba70c9c96b35
Sumanth Paranjape <sparanj2@asu.edu>
2020-04-13 18:42:48 -0700

    US#57 | Task#70: Working code, derived classes Radar and Range Sensor bug fixes

--- Sensors/Assets/Scenes/Scripts/RadarSensor.cs
+++ Sensors/Assets/Scenes/Scripts/RadarSensor.cs
@@ -9,124 +9,109 @@
 using UnityEngine;
 class RadarSensor : Sensors
 {
-    public override void update_Obstacles(GameObject gObj){
-        //.......change
-        // initial position for every step before checking for potential collisions
-        // 5X5 matrix is taken for this sensor
-        // -1 : don't know; setting distance of collision to object in range of (1f-2f)
+    public RadarSensor(){
         sensorLength = 4f;
-        DrawCircle(gObj.transform.position, sensorLength, Color.blue);
-        DrawCircle(gObj.transform.position, sensorLength-2, Color.red);
-
-
-        this.obstacle_matrix = new int[,] {{ -1, -1, -1, -1, -1 },
+        obstacle_matrix = new int[,] {{ -1, -1, -1, -1, -1 },
                                   { -1, -1, -1, -1, -1 },
                                   { -1, -1,  2, -1, -1 },
                                   { -1, -1, -1, -1, -1 },
                                   { -1, -1, -1, -1, -1 }};
+    }
+    public override void update_Obstacles(GameObject gObj){
+        /*.......change
+        initial position for every step before checking for potential collisions
+        5X5 matrix is taken for this sensor
+        -1 : don't know; setting distance of collision to object in range of (1f-2f) */
+    
+        DrawCircle(gObj.transform.position, sensorLength, Color.blue);
+        DrawCircle(gObj.transform.position, sensorLength-2, Color.red);
 
         // Straight Lines
         checkObstacle(gObj.transform.position,
                       Vector3.forward,
                       gObj, 0, "Front",
                       new int[] { 0, 2 },
-                      new int[] { 1, 2 },
-                      this.obstacle_matrix);
+                      new int[] { 1, 2 });
         checkObstacle(gObj.transform.position,
                       -Vector3.forward,
                       gObj, 0, "Back",
                       new int[] { 4, 2 },
-                      new int[] { 3, 2 },
-                      this.obstacle_matrix);
+                      new int[] { 3, 2 });
         checkObstacle(gObj.transform.position,
                       Vector3.right,
                       gObj, 0, "Right",
                       new int[] { 2, 4 },
-                      new int[] { 2, 3 },
-                      this.obstacle_matrix);
+                      new int[] { 2, 3 });
         checkObstacle(gObj.transform.position,
                       -Vector3.right,
                       gObj, 0, "Left",
                       new int[] { 2, 0 },
-                      new int[] { 2, 1 },
-                      this.obstacle_matrix);
+                      new int[] { 2, 1 });
 
         // 45 Degree Angle Lines
         checkObstacle(gObj.transform.position,
                       Quaternion.AngleAxis(45, Vector3.up) * gObj.transform.forward,
                       gObj, 45, "45 Degrees Front Right",
                       new int[] { 1, 3 },
-                      new int[] { 1, 3 },
-                      this.obstacle_matrix);
+                      new int[] { 1, 3 });
         checkObstacle(gObj.transform.position,
                       Quaternion.AngleAxis(-45, Vector3.up) * gObj.transform.forward,
                       gObj, -45, "45 Degrees Front Left",
                       new int[] { 1, 1 },
-                      new int[] { 1, 1 },
-                      this.obstacle_matrix);
+                      new int[] { 1, 1 });
         checkObstacle(gObj.transform.position,
                       Quaternion.AngleAxis(-45, Vector3.up) * (-gObj.transform.forward),
                       gObj, -45, "45 Degrees Back Left",
                       new int[] { 3, 1 },
-                      new int[] { 3, 1 },
-                      this.obstacle_matrix);
+                      new int[] { 3, 1 });
         checkObstacle(gObj.transform.position,
                       Quaternion.AngleAxis(45, Vector3.up) * (-gObj.transform.forward),
                       gObj, 45, "45 Degrees Back Right",
                       new int[] { 3, 3 },
-                      new int[] { 3, 3 },
-                      this.obstacle_matrix);
+                      new int[] { 3, 3 });
         checkObstacle(gObj.transform.position,
                       Quaternion.AngleAxis(25, Vector3.up) * gObj.transform.forward,
                       gObj, 25, "25 Degrees Front Right",
                       new int[] { 0, 3 },
-                      new int[] { 0, 3 },
-                      this.obstacle_matrix);
+                      new int[] { 0, 3 });
         checkObstacle(gObj.transform.position,
                       Quaternion.AngleAxis(-25, Vector3.up) * gObj.transform.forward,
                       gObj, -25, "25 Degrees Front Left",
                       new int[] { 0, 1 },
-                      new int[] { 0, 1 },
-                      this.obstacle_matrix);
+                      new int[] { 0, 1 });
 
         
         checkObstacle(gObj.transform.position,
                       Quaternion.AngleAxis(-25, Vector3.up) * gObj.transform.right,
                       gObj, -25, "25 Degrees Right Up",
                       new int[] { 1, 4 },
-                      new int[] { 1, 4 },
-                      this.obstacle_matrix);
+                      new int[] { 1, 4 });
         checkObstacle(gObj.transform.position,
                       Quaternion.AngleAxis(25, Vector3.up) * gObj.transform.right,
                       gObj, 25, "25 Degrees Right Down",
                       new int[] { 3, 4 },
-                      new int[] { 3, 4 },
-                      this.obstacle_matrix);
+                      new int[] { 3, 4 });
 
         checkObstacle(gObj.transform.position,
                       Quaternion.AngleAxis(25, Vector3.up) * (-gObj.transform.right),
                       gObj, 25, "25 Degrees left Up",
                       new int[] { 1, 0 },
-                      new int[] { 1, 0 },
-                      this.obstacle_matrix);
+                      new int[] { 1, 0 });
         checkObstacle(gObj.transform.position,
                       Quaternion.AngleAxis(-25, Vector3.up) * (-gObj.transform.right),
                       gObj, -25, "25 Degrees left Down",
                       new int[] { 3, 0 },
-                      new int[] { 3, 0 },
-                      this.obstacle_matrix);
+                      new int[] { 3, 0 });
 
         checkObstacle(gObj.transform.position,
                       Quaternion.AngleAxis(25, Vector3.up) * (-gObj.transform.forward),
                       gObj, -25, "25 Degrees Back Right",
                       new int[] { 4, 3 },
-                      new int[] { 4, 3 },
-                      this.obstacle_matrix);
+                      new int[] { 4, 3 });
         checkObstacle(gObj.transform.position,
                       Quaternion.AngleAxis(-25, Vector3.up) * (-gObj.transform.forward),
                       gObj, 25, "25 Degrees Back Left",
                       new int[] { 1, 3 },
-                      new int[] { 1, 3 },
-                      this.obstacle_matrix);
+                      new int[] { 1, 3 });
      }
 }
