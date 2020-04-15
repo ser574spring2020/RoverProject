c424172362a336d7dda42ed0ed1e310b0e878d72
aneeshdalvi <adalvi1@asu.edu>
2020-04-14 21:49:45 -0700

    Updated dll library file for minor fix

--- Sensors/Assets/Scenes/Scripts/SensorController.cs
+++ Sensors/Assets/Scenes/Scripts/SensorController.cs
@@ -5,6 +5,7 @@
     Email    : adalvi1@asu.edu | Arizona State University.
 */
 
+using System;
 using UnityEngine;
 
 /// <summary>
@@ -21,61 +22,78 @@ public class SensorController : MonoBehaviour
 
     private static int sensorType;
     public GameObject Cube;
-    void Start(){
+    void Start() {
         sensor = SensorsComponent.SensorFactory.GetInstance(getSensorType(), Cube);
         Debug.Log(sensor.GetCurrentSensor());
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void testProximityMatrix(int[,] matrix)
     {
+        {
+            Debug.Log("Printing Matrix : -- ");             
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Debug.Log(string.Format("{0} ", matrix[i, j]));
+                }                
+                Debug.Log("-- row done -- ");
+            }            
+        }
+    }
+
+        // Update is called once per frame
+        void Update()
+        {
         changePosRover();
-        sensor.Update_Obstacles(Cube);        
+        sensor.Update_Obstacles(Cube);
         sensor.SetSensorType(getSensorType());
         int[,] matrix = sensor.Get_Obstacle_Matrix();
-        
+        testProximityMatrix(matrix);
 
         //Debug.Log("Getting matrix of Size:"+matrix.Length);
 
     }
 
-    private void changePosRover()
-    {
-        float speed = 10f;
-        if (Input.GetKey(KeyCode.W))
-            Cube.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        private void changePosRover()
+        {
+            float speed = 10f;
+            if (Input.GetKey(KeyCode.W))
+                Cube.transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
-        if (Input.GetKey(KeyCode.S))
-            Cube.transform.Translate(-1 * Vector3.forward * Time.deltaTime * speed);
+            if (Input.GetKey(KeyCode.S))
+                Cube.transform.Translate(-1 * Vector3.forward * Time.deltaTime * speed);
 
-        if (Input.GetKey(KeyCode.A))
-            Cube.transform.Rotate(0, -1, 0);
+            if (Input.GetKey(KeyCode.A))
+                Cube.transform.Rotate(0, -1, 0);
 
-        if (Input.GetKey(KeyCode.D))
-            Cube.transform.Rotate(0, 1, 0);
-    }
+            if (Input.GetKey(KeyCode.D))
+                Cube.transform.Rotate(0, 1, 0);
+        }
 
-    public void setSensorType(int value)
-    {
-        sensorType = value;        
-    }
+        public void setSensorType(int value)
+        {
+            sensorType = value;
+        }
 
-    private int getSensorType()
-    {
-        return sensorType;
-    }
+        private int getSensorType()
+        {
+            return sensorType;
+        }
 
-    /*private void highlightCollider(GameObject gObj)
-    {
-        Renderer r = gObj.GetComponent(typeof(Renderer)) as Renderer;
-        oldColor = r.material.GetColor("_Color");
-        Color newColor = new Color(oldColor.r + 0.2f, oldColor.g + 0.2f, oldColor.b + 0.2f, oldColor.a);
-        r.material.SetColor("_Color", newColor);
-    }
+        /*private void highlightCollider(GameObject gObj)
+        {
+            Renderer r = gObj.GetComponent(typeof(Renderer)) as Renderer;
+            oldColor = r.material.GetColor("_Color");
+            Color newColor = new Color(oldColor.r + 0.2f, oldColor.g + 0.2f, oldColor.b + 0.2f, oldColor.a);
+            r.material.SetColor("_Color", newColor);
+        }
 
-    private void restoreColor(GameObject gObj)
-    {
-        Renderer r = gObj.GetComponent(typeof(Renderer)) as Renderer;
-        r.material.SetColor("_Color", oldColor);
-    }*/
+        private void restoreColor(GameObject gObj)
+        {
+            Renderer r = gObj.GetComponent(typeof(Renderer)) as Renderer;
+            r.material.SetColor("_Color", oldColor);
+        }*/
 }
+
