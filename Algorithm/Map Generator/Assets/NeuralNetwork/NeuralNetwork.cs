227d9ff14f9ead16666501dcf6ddf90304855085
MayankBatra005 <er.mayank005@gmail.com>
2020-04-22 01:26:06 -0700

    US#86 Task#98 NN Integration Completed

---
+++ Algorithm/Map Generator/Assets/NeuralNetwork/NeuralNetwork.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace NeuralNet
+{
+    public class NeuralNetwork 
+    {
+        private string nextCommands;
+
+      public  NeuralNetwork()
+        {
+            nextCommands = "";
+        }
+    
+        public string getNextCommands()
+        {
+            return nextCommands=NeuralNetwork.NeuralNEtworkConnection();
+        }
+        
+        static string NeuralNEtworkConnection()
+        {
+           
+            var psi = new ProcessStartInfo();
+            // Python path and Neural Network Script path 
+            psi.FileName = @"C:\Users\Mayank PC\AppData\Local\Programs\Python\Python38-32\python.exe";
+            var script = @"F:\NN.py";
+            var testData = " ";
+            var end = " ";
+            psi.Arguments = $"\"{script}\" \"{testData}\" \"{end}\"";
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+            var errors = "";
+            var results = "";
+            using (var process = Process.Start(psi))
+            {
+                errors = process.StandardError.ReadToEnd();
+                results = process.StandardOutput.ReadToEnd();
+            }
+            
+            return results;
+        }
+    }
+}
